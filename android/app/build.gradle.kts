85d9ad27202fb6b4bae9ca828f7dc057f4287806
WingedSaga <vovamorkova562@gmail.com>
2026-08-10 20:20:24 +0300

    Add Android News Seconds client

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+  id("com.android.application")
+}
+
+android {
+  namespace = "org.newsseconds.app"
+  compileSdk = 36
+
+  defaultConfig {
+    applicationId = "org.newsseconds.app"
+    minSdk = 26
+    targetSdk = 36
+    versionCode = 1
+    versionName = "1.0.0"
+  }
+}
